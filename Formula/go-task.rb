e90df0589912dca8cf8c5e1e041ca9ea5e154086
Andrey Nering <andrey.nering@gmail.com>
2018-03-07 23:21:30 -0300

    Update checksum for v2.0.0

--- Formula/go-task.rb
+++ Formula/go-task.rb
@@ -3,7 +3,7 @@ class GoTask < Formula
   homepage "https://github.com/go-task/task"
   url "https://github.com/go-task/task/releases/download/v2.0.0/task_darwin_amd64.tar.gz"
   version "2.0.0"
-  sha256 "4534c408124c13746ed70cdfb74e4853bef24705f5e9b81149999e4f4a46be46"
+  sha256 "8fb3e8a5fcb7eda011239a37455a3a55e8aad51c3916ecd9b9e586474e3becfe"
 
   def install
     bin.install "task"
