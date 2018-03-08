2f543277f799d0c3b91061d935ee0af5acff73ad
Andrey Nering <andrey.nering@gmail.com>
2018-03-07 23:19:27 -0300

    Release v2.0.0

--- Formula/go-task.rb
+++ Formula/go-task.rb
@@ -1,8 +1,8 @@
 class GoTask < Formula
   desc "Task runner / simpler Make alternative written in Go"
   homepage "https://github.com/go-task/task"
-  url "https://github.com/go-task/task/releases/download/v1.4.4/task_darwin_amd64.tar.gz"
-  version "1.4.4"
+  url "https://github.com/go-task/task/releases/download/v2.0.0/task_darwin_amd64.tar.gz"
+  version "2.0.0"
   sha256 "4534c408124c13746ed70cdfb74e4853bef24705f5e9b81149999e4f4a46be46"
 
   def install
