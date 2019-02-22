950d1c612f1065dc031c766f3937077853927f9a
Andrey Nering <andrey.nering@gmail.com>
2019-02-21 21:48:18 -0300

    Bump v2.4.0

--- Formula/go-task.rb
+++ Formula/go-task.rb
@@ -1,9 +1,9 @@
 class GoTask < Formula
   desc "Task runner / simpler Make alternative written in Go"
   homepage "https://github.com/go-task/task"
-  url "https://github.com/go-task/task/releases/download/v2.3.0/task_darwin_amd64.tar.gz"
-  version "2.3.0"
-  sha256 "075021b40c9edf7231c3013d0ab46e68bd039cd44b6c1e82061b5544eb06cdf9"
+  url "https://github.com/go-task/task/releases/download/v2.4.0/task_darwin_amd64.tar.gz"
+  version "2.4.0"
+  sha256 "c4b633e8d343314841c663ff350b2766695f05038a01429860baefef6dba3a51"
 
   def install
     bin.install "task"
