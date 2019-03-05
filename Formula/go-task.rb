b764ef9f83e43b9524411a06c54bf4d448ae3ce7
Andrey Nering <andrey.nering@gmail.com>
2019-03-04 23:29:36 -0300

    Update homepage

--- Formula/go-task.rb
+++ Formula/go-task.rb
@@ -1,6 +1,6 @@
 class GoTask < Formula
   desc "Task runner / simpler Make alternative written in Go"
-  homepage "https://github.com/go-task/task"
+  homepage "https://taskfile.dev"
   url "https://github.com/go-task/task/releases/download/v2.4.0/task_darwin_amd64.tar.gz"
   version "2.4.0"
   sha256 "c4b633e8d343314841c663ff350b2766695f05038a01429860baefef6dba3a51"
