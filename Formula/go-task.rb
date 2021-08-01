496676c306be87c24e7d0ee0472e575a94dc4a81
Andrey Nering <andrey@nering.com.br>
2021-07-31 21:44:57 -0300

    Bump v3.7.0

--- Formula/go-task.rb
+++ Formula/go-task.rb
@@ -1,14 +1,14 @@
 class GoTask < Formula
   desc "Task runner / simpler Make alternative written in Go"
   homepage "https://taskfile.dev"
-  version "3.6.0"
+  version "3.7.0"
 
   if OS.mac?
     url "https://github.com/go-task/task/releases/download/v#{version}/task_darwin_amd64.tar.gz"
-    sha256 "c2aebbaec123b4cf80d61c399c73c380cdda89b683de8883154f3cc419d29eb0"
+    sha256 "6c03a1110561d2954e3be56ff012c470612fe2f2fec7a86db3782897ed8be434"
   elsif OS.linux?
     url "https://github.com/go-task/task/releases/download/v#{version}/task_linux_amd64.tar.gz"
-    sha256 "8c39aa76aa455d0a38b61e424f6b857bbb7ef896423c8e432f49f0d221cf05f2"
+    sha256 "3d3a2651fc28344dc0aea4291a72787a4b8bd172df95d59baa7f14e5bd0f7b2f"
   end
 
   def install
